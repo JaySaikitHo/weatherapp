1a9bd4b2d9ae8c7dfcc8915c8e8e59796f6be60b
Jay <ttjho@hotmail.com>
2021-01-17 18:29:41 -0700

    add graph and dependency with react-vs

--- weatherapp/src/hooks/Getforecast.js
+++ weatherapp/src/hooks/Getforecast.js
@@ -1,12 +1,8 @@
 
 export default function Getforecast(city) {
-  // const [dailyData, setDailyData] = useState([]);
+  
   const test = "9fc18a0269d603fe395f54a64d2f07ec"
   
-  let dailyData = [];
-  if(!city) {
-    return dailyData;
-  }
   let lat = "0";
   let long = "0";
   if (city === "Vancouver") {
@@ -21,11 +17,8 @@ export default function Getforecast(city) {
   }
   return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=current,minutely,hourly,alerts&units=metric&appid=${test}`)
     .then(response => response.json())
-    .then(response => {
-      console.log("response",response)
-      dailyData = response;
-      return dailyData;
-    })   
-   
+
+
+  
 }
 
--- weatherapp/src/weather.js
+++ weatherapp/src/weather.js
@@ -1,63 +1,121 @@
 
 import React, { useEffect, useState } from "react";
+import '../node_modules/react-vis/dist/style.css';
+import { XYPlot, LineSeries, HorizontalGridLines, VerticalGridLines, XAxis, YAxis } from 'react-vis';
+import moment from 'moment';
 import Getcurrentweather from './hooks/Getcurrentweather';
 import Getforecast from './hooks/Getforecast';
 import Temperature from './components/Temperature';
 import Humidity from './components/Humidity';
 import Windspeed from './components/Windspeed';
-
+const get = require('lodash.get');
 
 export default function Main() {
-  
- 
-  let forecastArray = [];
-  
-  const [city, setCity] = useState("");
-  const [dailyForecast, setDailyForecast] = useState([])
-  
-  const onclick_setCity = function(event) {
-    setCity(event.target.value)
-    
-  }
-  
-  function fillArray (city) {
-    forecastArray.push(Getforecast(city))
-  }
-  
-  fillArray(city);
-  useEffect(()=> {
-    Promise.all(forecastArray)
-    .then((data) => {setDailyForecast(data[0].daily)})
 
-  },[city])
-  
-  console.log("dailyForecast", dailyForecast)
-  
-  
+  const [graph, setGraph] = useState(
+  [
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+  ]
+);
+
+
+  let forecastArray = [];
+
+  const [city, setCity] = useState("");
+  const [dailyForecast, setDailyForecast] = useState([]);
+
+  const onclick_setCity = function (event) {
+    setCity(event.target.value);
+
+  };
+
+
+
+
+  useEffect(() => {
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    function Getdata(city) {
+      //  console.log('getdata', city)
+
+      if (city) {
+        Getforecast(city)
+          .then(data => {
+            console.log("data from useeffect", data);
+            if (data) {
+              console.log('graph', data.daily[0].dt);
+              var dailies = get(data, 'daily', []);
+              
+              for (var i = 0; i < 7; i++) {
+                dailies[i].x = weekdays[new Date(dailies[i].dt * 1000).getDay()] 
+                dailies[i].y = get(dailies[i], 'temp.max', 0)
+              }
+
+              // dailies = dailies.map((day) => ({
+              //   x: weekdays[new Date(day.dt * 1000).getDay()],
+              //   y: get(day, 'temp.max', 0)
+              // }));
+
+              setGraph(dailies);
+            }
+          });
+      }
+
+    }
+
+    Getdata(city);
+  }, [city]);
+
+  /*
+  function getForecastTest(city ) {
+    return fetch('https://api.openweathermap.org/data/2.5/onecall?lat=49.2827&lon=-123.1207&exclude=current,minutely,hourly,alerts&units=metric&appid=9fc18a0269d603fe395f54a64d2f07ec')
+      .then(response => response.json())
+  }
+  */
+
+
+  console.log("dailyForecast", dailyForecast);
+
   const currentWeather = Getcurrentweather(city);
-  console.log("currentweather",currentWeather)
-  
-  
-    
-  
-  
+  console.log("currentweather", currentWeather);
+
+
+
+
+
   return (
     <div>
       <h2>Choose a city</h2>
       <select onChange={onclick_setCity}>
-              <option className="location_option" value="location">Choose location
+        <option className="location_option" value="location">Choose location
               </option>
 
-                <option value="Calgary">
-                Calgary
+        <option value="Calgary">
+          Calgary
                 </option>
-                <option value="Vancouver">Vancouver</option>
-                <option value="Toronto">Toronto</option>
-              </select>
-      
-      <Temperature currentTemperature = {currentWeather.temperature} />
-      <Windspeed currentWindspeed = {currentWeather.windSpeed} />
-      <Humidity currentHumidity = {currentWeather.humidity} />
+        <option value="Vancouver">Vancouver</option>
+        <option value="Toronto">Toronto</option>
+      </select>
+
+      <Temperature currentTemperature={currentWeather.temperature} />
+      <Windspeed currentWindspeed={currentWeather.windSpeed} />
+      <Humidity currentHumidity={currentWeather.humidity} />
+      <h2>7 day Forecast</h2>
+      <div className="graph">
+        <XYPlot height={300} width={700} stroke="red" xType="ordinal">
+          <LineSeries data={graph} />
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis title="Day" />
+          <YAxis title="Temp degrees Celsius" />
+        </XYPlot>
+      </div>
     </div>
-  )
+  );
 }
